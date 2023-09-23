f536b4aae5c31e88e4d33ff8d25d71a50e260085
Brigitte Utria <brigittekari@hotmail.com>
2023-09-22 19:30:42 -0500

    Ordenar la data filtrada

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import { filterBy,ordenarPokemon } from './dataFunctions.js';
 
 const root = document.querySelector('#root')
 const dataPokemon = data.pokemon;
+let isDataFiltered = false; //rastrea si la data esta filtrada
+let pokemonFilter = [];
+
 root.appendChild(renderItems(dataPokemon));
 
 const filtrarButton = document.getElementById('filtrar-button');
@@ -18,16 +21,16 @@ filtrarButton.addEventListener('click', () => {
 
 selectRegion.addEventListener('change', () => {
   const selectedRegionOption = selectRegion.value;
-  const pokemonFilter = filterBy(dataPokemon, 'region', selectedRegionOption);
-
+  pokemonFilter = filterBy(dataPokemon, 'region', selectedRegionOption);
+  isDataFiltered = true; // Marcar que la data está filtrada
   renderItems(pokemonFilter);
   filtrarOptions.classList.remove('mostrar');
 });
 
 selectType.addEventListener('change', () => {
   const selectedTypeOption = selectType.value;
-  const pokemonFilter = filterBy(dataPokemon, 'tipo', selectedTypeOption);
-
+  pokemonFilter = filterBy(dataPokemon, 'tipo', selectedTypeOption);
+  isDataFiltered = true; 
   renderItems(pokemonFilter );
   filtrarOptions.classList.remove('mostrar');
 });
@@ -39,15 +42,30 @@ const ordenarOptions = document.getElementById('ordenar-options');
 
 ordenarButton.addEventListener('click', () => {
   ordenarOptions.classList.toggle('mostrar');
-});
 
-ordenarOptions.addEventListener('click', (event) => {
-  if (event.target.tagName === 'BUTTON') {
-    const selectedOption = event.target.getAttribute('data-orden');
-    const [property, order] = selectedOption.split('-');
+//Verificar si la data esta filtrada y proceder a ordenarla
+  if (isDataFiltered) {
+    ordenarOptions.addEventListener('click', (event) => {
+     if (event.target.tagName === 'BUTTON') {
+      const selectedOption = event.target.getAttribute('data-orden');
+      const [property, order] = selectedOption.split('-');
 
-    const sortedPokemon = (ordenarPokemon(dataPokemon, property, order));
-    renderItems( sortedPokemon );
-    ordenarOptions.classList.remove('mostrar');
-  }
-});
+      const sortedPokemon = ordenarPokemon(pokemonFilter, property, order);
+      renderItems(sortedPokemon);
+      ordenarOptions.classList.remove('mostrar');
+     }   
+  });
+  } else {
+    ordenarOptions.addEventListener('click', (event) => {
+      if (event.target.tagName === 'BUTTON') {
+        const selectedOption = event.target.getAttribute('data-orden');
+        const [property, order] = selectedOption.split ('-');
+
+        const sortedPokemon = ordenarPokemon(dataPokemon, property, order);
+        renderItems(sortedPokemon);
+        ordenarOptions.classList.remove('mostrar');
+      }
+    });
+   } 
+  });
+
